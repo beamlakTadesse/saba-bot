c39235e61c321ec4e5be54cfff1d816b5c730ebb
SF Studio plc <beamlaktadesse5@gmail.com>
2023-12-22 19:25:47 +0300

    fixed role translation

--- src/scenes/roleScene.js
+++ src/scenes/roleScene.js
@@ -5,26 +5,43 @@ const BaseScene = require("telegraf/scenes/base");
 
 // Scene for selecting user role
 const roleScene = new BaseScene("role");
+const setLan = (ctx) => {
+  const language = ctx.session.language;
 
+  if (language == "english") {
+    ctx.i18n.locale("en");
+  } else if (language == "amharic") {
+    ctx.i18n.locale("am");
+  } else if (language == "afaan oromo") {
+    ctx.i18n.locale("or");
+  } else if (language == "tigrgna") {
+    ctx.i18n.locale("tr");
+  } else {
+    ctx.i18n.locale("en");
+  }
+};
 roleScene.enter((ctx) => {
-ctx.reply(ctx.i18n.t("intro"))
+  ctx.reply(ctx.i18n.t("intro"));
 
   ctx.reply(ctx.i18n.t("Are you a Doctor or a Patient?"), {
-    reply_markup: Markup.keyboard([["Doctor", "Patient"]])
+    reply_markup: Markup.keyboard([
+      [ctx.i18n.t("Doctor"), ctx.i18n.t("Patient")],
+    ])
       .resize()
       .oneTime(),
   });
 });
 
-roleScene.hears(/^(doctor|patient)$/i, async (ctx) => {
-  const role = ctx.match[1].toLowerCase();
-
+roleScene.hears(/^(.*)$/, async (ctx) => {
+  const role = ctx.match[1];
+  setLan(ctx);
   ctx.session.role = role;
-  if (role === "patient") {
+  console.log(ctx.i18n.t("Patient"));
+  if (role === ctx.i18n.t("Patient")) {
     // Transition to the getNameScene
     return ctx.scene.enter("getAge");
   }
-  if (role == "doctor") {
+  if (role == ctx.i18n.t("Doctor")) {
     return ctx.scene.enter("doctorName");
   }
 
